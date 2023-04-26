9265ca88d26f229f6265e4a7271fffc34006eb39
Marina <marina.zilkina@gmail.com>
2023-04-26 17:15:55 +0300

    auth component is improved

--- src/components/AuthorizationComponent/AuthorizationComponent.jsx
+++ src/components/AuthorizationComponent/AuthorizationComponent.jsx
@@ -1,33 +1,42 @@
 import './AuthorizationComponent.css';
+import {Link} from "react-router-dom";
+import MainPageComponent from "../MainPageComponent/MainPageComponent";
 
 function AuthorizationComponent () {
     return (
         <div className="auth-component">
             <div className="wrapper">
                 <div className="pic-lock"></div>
-            <div className="auth-upper-side">
-                <div className="auth-enter_div">
-                    <a id="auth-enter">Войти</a>
-                <div className="auth-separator"></div>
+                <div className="tabs">
+                    <div className="tab">
+                        <input type="radio" id="tab1" name="tab-group" checked/>
+                            <label htmlFor="tab1" className="tab-title">Войти
+                                <div className="auth-separator"></div>
+                            </label>
+
+                            <section className="tab-content">
+                                <div className="auth-center-side">
+                                    <div className="auth-input-title"> Логин или номер телефона:</div>
+                                    <input id="input" type="text" size="30"/>
+                                    <div className="auth-input-title"> Пароль:</div>
+                                    <input id="input" type="password"/>
+                                    <div className="auth-button">
+                                        <Link to="/" id="auth-text">Войти</Link>
+                                    </div>
+                                    <div className="auth-restore-pass">
+                                        <a href="#" id="text-auth">Восстановить пароль</a>
+                                    </div>
+                                </div>
+                            </section>
+                    </div>
+                    <div className="tab">
+                        <input type="radio" id="tab2" name="tab-group"/>
+                            <label htmlFor="tab2"  id="tab-2" className="tab-title">Зарегистрироваться
+                                <div className="auth-separator1"></div>
+                            </label>
+                            <section className="tab-content">Регистрации в задании не было</section>
+                    </div>
                 </div>
-                <div className="auth-reg">
-                    <a id="auth-reg">Зарегистрироваться</a>
-                <div className="auth-separator1"></div>
-                </div>
-            </div>
-            <div className="auth-center-side">
-                <div className="auth-input-title"> Логин или номер телефона:</div>
-                <input id="input" type="text" size="30"/>
-                <div className="auth-input-title"> Пароль:</div>
-                <input id="input" type="password"/>
-                <div className="auth-button">
-                    <a href="#" id="auth-text">Войти</a>
-                </div>
-                {/*<button className="auth-button"><span id="auth-text">Войти</span></button>*/}
-                <div className="auth-restore-pass">
-                    <a href="#" id="text-auth">Восстановить пароль</a>
-                </div>
-            </div>
             <div className="auth-lower-side">
                 <p id="p">Войти через:</p>
                 <div className="auth-logos">
