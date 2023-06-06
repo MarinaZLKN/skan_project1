62364d39c635210fa2b3da5aa3e6f49eb58b080c
Marina <marina.zilkina@gmail.com>
2023-06-06 21:39:34 +0300

    result-card css

--- src/components/ResultPage/ResultCard/ResultCard.jsx
+++ src/components/ResultPage/ResultCard/ResultCard.jsx
@@ -2,7 +2,17 @@ import './ResultCard.css';
 
 function ResultCard() {
     return (
-        <div className="result-card"> card</div>
+        <div className="result-card">
+            <div className="result-card_header">
+                <label className="result-card_date"> date</label>
+                <label className="result-card_source"> source</label>
+            </div>
+            <div className="result-card_title"></div>
+            <div className="result-card_news-type"></div>
+            <div className="result-card_pic"></div>
+            <div className="result-card_text"></div>
+            <button className="result-card_btn">Читать в источнике</button>
+        </div>
     )
 }
 
